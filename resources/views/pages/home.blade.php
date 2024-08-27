129869fd8c4b87a43874fdb4165240daa0e90173
chidokato <tuan.pn92@gmail.com>
2024-08-27 14:18:06 +0700

    update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -62,7 +62,7 @@
 
         <div class="load-more text-center mt-4 pt-2">
             <div class="cta-btn ir">
-                <a class="" href="#"><span class="cta-text font-weight-semibold">Xem tất cả</span><span class="cta-ico"><i class="icon-next"></i></span></a>
+                <a class="" target="_blank" href="du-an"><span class="cta-text font-weight-semibold">Xem tất cả</span><span class="cta-ico"><i class="icon-next"></i></span></a>
             </div>
         </div>
     </div>
