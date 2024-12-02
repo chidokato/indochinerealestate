60d66fe95714e0c104a0e4cb77540c8354dd954c
chidokato <tuan.pn92@gmail.com>
2024-12-02 15:16:42 +0700

    sdf

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,6 +4,7 @@
 @section('description') {{$setting->description}} @endsection
 @section('robots') index, follow @endsection
 @section('url'){{asset('')}}@endsection
+@section('img'){{asset('')}}assets/images/cover.jpg@endsection
 
 @section('css')
 <link href="assets/css/home.css" rel="stylesheet">
