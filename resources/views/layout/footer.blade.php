b65810df1b2734c81e8e94594a7e6de90fad0147
chidokato <tuan.pn92@gmail.com>
2025-01-23 15:18:09 +0700

    sdf

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -29,9 +29,9 @@
                 </div>
                 <div class="ft-social">
                     <ul class="social">
-                        <li><a class=""><i class="icon-facebook"></i></a></li>
-                        <li><a class=""><i class="icon-youtube"></i></a></li>
-                        <li><a class=""><i class="icon-zalo"></i></a></li>
+                        <li><a class="{{$setting->facebook}}"><i class="icon-facebook"></i></a></li>
+                        <li><a class="{{$setting->youtube}}"><i class="icon-youtube"></i></a></li>
+                        <!-- <li><a class=""><i class="icon-zalo"></i></a></li> -->
                     </ul>
                 </div>
             </div>
