ed36bad6a0883bbed5b57147ce0494b293e4c75a
chidokato <tuan.pn92@gmail.com>
2025-01-23 15:12:41 +0700

    sdf

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -4,6 +4,7 @@
 @section('description') {{$post->description ? $post->description : $post->name.$post->name}} @endsection
 @section('robots') index, follow @endsection
 @section('url'){{asset('')}}@endsection
+@section('img'){{asset('')}}data/images/{{$val->img}} @endsection
 
 @section('css')
 <link href="assets/css/widget.css" rel="stylesheet">
