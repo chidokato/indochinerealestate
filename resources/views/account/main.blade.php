ea88249616fe56b97b427d7fa86bd9e10f502cf7
chidokato <tuan.pn92@gmail.com>
2025-09-27 10:39:52 +0700

    sdf

--- resources/views/account/main.blade.php
+++ resources/views/account/main.blade.php
@@ -7,31 +7,12 @@
 
 @section('content')
 
-<form action="{{ url()->current() }}" method="GET">
-<section class="sec-fiter-search floating-label">
-    <div class="container">
-        <div data-bs-toggle="button" class="d-md-none"><button type="button" class="btn btn-circle btn-toggle"><span class="icon-search"></span></button></div>
-        
-            <div class="row g-3 justify-content-lg-end">
-                <div class="col-lg-4">
-                    <div class="input-group search-input">
-                        <span class="input-group-text border100"><i class="icon-search"></i></span>
-                        <input value="" name="key" type="text" class="form-control" placeholder="Nhập địa chỉ, dự án">
-                    </div>
-                    <button type="submit" class="btn btn-circle">Tìm kiếm</button>
-                </div>
-            </div>
-        
-    </div>
-</section>
-
-<!------------------- CARD ------------------->
 <section class="card-grid sales-sec list-tindang">
     <div class="container">
         <div class="row">
-            <div class="col-lg-3 d-none d-lg-block">
+            <div class="col-lg-3">
                 <div class="widget widget-list widget-hightlight mb-3">
-                    <h4><span>Loại hình</span></h4>
+                    <h4><span>Dạng mục</span></h4>
                 	<div class="form-check">
                         <input class="form-check-input" type="checkbox" name="categories[]">
                         <label class="form-check-label aa22" for="flexCheck1">
@@ -49,8 +30,6 @@
         </div>
     </div>
 </section>
-<!------------------- END CARD ------------------->
-</form>
 
 @endsection
 
